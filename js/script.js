a7eb8f7fc45987d573035567ff737681d802a8b3
ANA <acurcija@Anas-MacBook-Air.local>
2015-12-01 10:14:43 -0800

    First Project github pages commit

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function jStaticLayout (image, description) {
-	var cardContainer = $("<div>").addClass("col-md-3");
-	var cardImages = $("<img>").attr("src", image);
-
-	cardContainer.append(cardImages);
-
-	return cardContainer;
-}
